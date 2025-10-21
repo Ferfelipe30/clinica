8a4b20f89b000ef74887348104f1b7838be8c2ca
Juan Felipe Fernandez <ferjuanfelipe7@gmail.com>
2025-10-20 22:14:27 -0500

    Actualizacion de frontend y backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api_app',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -60,6 +68,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:8000/api';
+
+export const api = axios.create({
+  baseURL: baseUrl,
+});
+
+export default api;
