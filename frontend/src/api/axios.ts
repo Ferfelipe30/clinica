b910bd0a4ee8c85a78c445d01b6a9f690bda06aa
Juan Felipe Fernandez <ferjuanfelipe7@gmail.com>
2025-11-04 11:48:41 -0500

    Actualizacion del env del frontend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL;
+
+console.log(import.meta.env.VITE_APP_NAME);
 
 export const api = axios.create({
-  baseURL: baseUrl,
+  baseURL: API_URL,
 });
 
 export default api;
